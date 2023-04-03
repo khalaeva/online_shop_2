88dd3176faf9778a1b93f6e57d25cddbb3ac8cd1
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-04-03 18:31:43 +0300

    m-wrpr

--- src/components/main-wrapper.vue
+++ src/components/main-wrapper.vue
@@ -1,33 +1,12 @@
 <template>
     <div class="v-main-wrapper">
         <RouterView></RouterView>
-        <!-- <Header/>
-        <Catalog/>
-        <Cart 
-            v-if="CART.length"
-            :cart_data="CART"/>  -->
     </div>
 </template>
 
 <script>
-// import Catalog from './catalog/catalog.vue'
-// import Header from './layout/header.vue'
-// import Cart from './cart/cart.vue'
-
-import { mapGetters } from 'vuex'
-
 export default {
-    name: 'v-main-wrapper',
-    // components: {
-    //     Catalog,
-    //     Header,
-    //     Cart
-    // },
-    computed: {
-        ...mapGetters([
-            'CART'
-        ])
-    }
+    name: 'v-main-wrapper'
 }
 </script>
 
