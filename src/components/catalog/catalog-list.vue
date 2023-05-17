49b740fb7512cc9ace826ae3ff39f51d12a67081
Anna <halaevaanna@gmail.com>
2023-05-17 11:47:48 +0300

    delete README

--- src/components/catalog/catalog-list.vue
+++ src/components/catalog/catalog-list.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        <button 
+            class="btn btn-secondary dropdown-toggle" 
+            type="button" 
+            id="dropdownMenuButton1" 
+            data-bs-toggle="dropdown" 
+            aria-expanded="false"
+            @click="drawTree">
           Каталог
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li v-for="category in CATEGORIES[0]" :key="category">
+            <li v-for="category in tree[0]" :key="category">
                 <RouterLink style="text-decoration: none" :to="{ name: 'type', params: { type: category.categoryId}}">
-                    <h4 class="dropdown-item">{{ category.nameCategory }}</h4>
+                    <h4 @click="setBrdcrm(category.nameCategory)" class="dropdown-item">{{ category.nameCategory }}</h4>
                 </RouterLink>
-                <Vtree :catId="category.categoryId"/> 
+                <Vtree 
+                    :tree="tree"
+                    :catId="category.categoryId"
+                    @setBrdcrm="setBrdcrm"/> 
             </li>
         </ul>
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 import Vtree from './v-tree.vue';
 
 export default {
@@ -22,12 +31,65 @@ export default {
     components: {Vtree},
     data() {
         return {
+            arr: ['Главная'],
+            tree: []
         }
     },
     computed: {
         ...mapGetters([
-            'CATEGORIES'
+            'CATEGORIES',
+            'BRDCRMS'
         ])
+    },
+    methods: {
+        ...mapActions([
+            'SET_BRDCRMS',
+            'CLEAR_BRDCRMS'
+        ]),
+        drawTree() {
+            this.tree = this.formTree(this.CATEGORIES)
+            console.log(this.tree)
+        },
+        formTree(arr){
+            let tree = []
+            for (let i in arr) {
+                if (arr[i].parentCategoryId === null)
+                {
+                    if(Array.isArray(tree[0])) {
+                        tree[0].push((arr[i]))
+                    }
+                    else {
+                        tree[0] = []
+                        tree[0].push((arr[i]))
+                    }
+                }
+                else {
+                    if(Array.isArray(tree[arr[i].parentCategoryId])) {
+                        tree[arr[i].parentCategoryId].push((arr[i]))
+                    }
+                    else {
+                        tree[arr[i].parentCategoryId] = []
+                        tree[arr[i].parentCategoryId].push((arr[i]))
+                    }
+                }
+            }
+            for (let i = 0; i < tree.length; i++) {
+                if (tree[i] === undefined) {
+                    tree[i] = 0
+                }
+            }
+            return tree
+        },
+        setBrdcrm(catName) {
+            this.CLEAR_BRDCRMS()
+            let brdcrm = this.CATEGORIES.find(el => el.nameCategory === catName)
+            while (brdcrm.parentCategoryId) {
+                this.SET_BRDCRMS(brdcrm.nameCategory)
+                brdcrm = this.CATEGORIES.find(el => el.categoryId === brdcrm.parentCategoryId)
+            }
+            this.this.SET_BRDCRMS(brdcrm.nameCategory)
+            console.log(this.BRDCRMS)
+        }
     }
 }
 </script>
